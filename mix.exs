0fc929426178070df2ecca3f664be893a1cfdbba
Mark Ericksen <brainlid@gmail.com>
2026-05-17 07:28:31 -0600

    prep for v0.8.0-rc.7 (#111)

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Sagents.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/sagents-ai/sagents"
-  @version "0.8.0-rc.6"
+  @version "0.8.0-rc.7"
 
   def project do
     [
